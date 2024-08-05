b5dc5e48c1e0ba8fca5d5e6748f84820ec8e4bad
Felipe Santos <159580121+felipesantos71@users.noreply.github.com>
2024-08-05 07:53:54 -0300

    Add files via upload

---
+++ Java3atv.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java3atv;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Java3atv {
+
+    public static void main(String[] args) {
+        // Declaração de variaveis
+        int a = 10;
+        int b = 2;
+        int maior;
+       
+        //Estrutura condicional if simples e operadores aritmeticos
+        if (a > b) {
+            maior = a; 
+        }   else    {
+            maior = b;
+        }
+        
+        //Exibindo resultados
+        System.out.println("O maior número é: " + maior);
+    }
+}
